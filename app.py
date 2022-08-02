e16d6ab3e7632b01f70959c4e1f2c6c1a83c0481
Srinivas <gsry99@gmail.com>
2022-08-01 22:47:08 -0500

    "added comments and removed unused files"

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
 import os
 import datetime
-import hashlib
+
 from flask import Flask, session, url_for, redirect, render_template, request, abort, flash
-import itsdangerous as it
-import markupsafe as mk
 
 import rospy
 from geometry_msgs.msg import Twist
 import sys, select, os
 if os.name == 'nt':
-  import msvcrt, time
+  import time
 else:
-  import tty, termios
+  import tty
 
+# Initialize the Flask webserver
 app = Flask(__name__)
-app.config.from_object('config')
+
+# Define the max linear velocity and max angular velocity.
 
 BURGER_MAX_LIN_VEL = 0.22
 BURGER_MAX_ANG_VEL = 2.84
@@ -25,14 +25,19 @@ WAFFLE_MAX_ANG_VEL = 1.82
 LIN_VEL_STEP_SIZE = 0.03
 ANG_VEL_STEP_SIZE = 0.1
 
+# Initialize turtlebot3_web
 
 rospy.init_node('turtlebot3_web')
+
+# Initialize the publisher to publish to the topic cmd_vel
 pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
 
+# Creating a turtlebot3 model
 turtlebot3_model = rospy.get_param("model", "burger")
 
 status = 0
 
+# initialize targer linear velocity and angular velocity with 0
 target_linear_vel   = 0.0
 target_angular_vel  = 0.0
 control_linear_vel  = 0.0
@@ -49,6 +54,7 @@ def constrain(input, low, high):
 
     return input
 
+# calculate the linear velocity based on the model type
 def checkLinearLimitVelocity(vel):
     if turtlebot3_model == "burger":
       vel = constrain(vel, -BURGER_MAX_LIN_VEL, BURGER_MAX_LIN_VEL)
@@ -59,6 +65,7 @@ def checkLinearLimitVelocity(vel):
 
     return vel
 
+# calculate the angular velocity based on the model type
 def checkAngularLimitVelocity(vel):
     if turtlebot3_model == "burger":
       vel = constrain(vel, -BURGER_MAX_ANG_VEL, BURGER_MAX_ANG_VEL)
@@ -69,6 +76,7 @@ def checkAngularLimitVelocity(vel):
 
     return vel
 
+# print the linear velocity and angular velocity
 def vels(target_linear_vel, target_angular_vel):
     return "currently:\tlinear vel %s\t angular vel %s " % (target_linear_vel,target_angular_vel)
 
@@ -82,7 +90,7 @@ def makeSimpleProfile(output, input, slop):
 
     return output
 
-
+# /up api to move the robot forward
 @app.route("/up", methods = ["POST"])
 def keyUp():
     print("Clicked on Up Button")
@@ -95,7 +103,7 @@ def keyUp():
     status=0
 
     target_linear_vel = checkLinearLimitVelocity(target_linear_vel + LIN_VEL_STEP_SIZE)
-    print("@@@@@@@@@@@@@@@@@ Up Before %s"%vels(target_linear_vel,target_angular_vel))
+    print("Moving the robot forward %s"%vels(target_linear_vel,target_angular_vel))
     status = status + 1
     twist = Twist()
 
@@ -103,14 +111,14 @@ def keyUp():
     twist.linear.x = control_linear_vel; twist.linear.y = 0.0; twist.linear.z = 0.0
     control_angular_vel = makeSimpleProfile(control_angular_vel, target_angular_vel, (ANG_VEL_STEP_SIZE/2.0))
     twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = control_angular_vel
+
+    # publishing the twist object to the topic with updated linear and angular velocity
     pub.publish(twist)
-    print("@@@@@@@@@@@@@@@@@ Up After %s"%vels(target_linear_vel,target_angular_vel))
     return "200"
 
-
+# /down api to move the robot backward
 @app.route("/down", methods = ["POST"])
 def keyDown():
-    print("Clicked on Down Button")
 
     target_linear_vel   = 0.0
     target_angular_vel  = 0.0
@@ -120,22 +128,25 @@ def keyDown():
     ANG_VEL_STEP_SIZE = 0.1
     status=0
     target_linear_vel = checkLinearLimitVelocity(target_linear_vel - LIN_VEL_STEP_SIZE)
-    print("@@@@@@@@@@@@@@@@@ Down Before %s"%vels(target_linear_vel,target_angular_vel))
+    print("Moving the robot backward %s"%vels(target_linear_vel,target_angular_vel))
     status = status + 1
 
+    # Create a twist object
     twist = Twist()
     control_linear_vel = makeSimpleProfile(control_linear_vel, target_linear_vel, (LIN_VEL_STEP_SIZE/2.0))
     twist.linear.x = control_linear_vel; twist.linear.y = 0.0; twist.linear.z = 0.0
 
     control_angular_vel = makeSimpleProfile(control_angular_vel, target_angular_vel, (ANG_VEL_STEP_SIZE/2.0))
     twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = control_angular_vel
+
+    # publishing the twist object to the topic with updated linear and angular velocity
     pub.publish(twist)
-    print("@@@@@@@@@@@@@@@@@ Down After %s"%vels(target_linear_vel,target_angular_vel))
     return "200"
 
+# /left api to move the robot anti clockwise
 @app.route("/left", methods = ["POST"])
 def keyLeft():
-    print("Clicked on Left Button")
+
     target_linear_vel   = 0.0
     target_angular_vel  = 0.0
     control_linear_vel  = 0.0
@@ -144,21 +155,23 @@ def keyLeft():
     ANG_VEL_STEP_SIZE = 0.1
     status=0
     target_angular_vel = checkAngularLimitVelocity(target_angular_vel + ANG_VEL_STEP_SIZE)
-    print("@@@@@@@@@@@@@@@@@ Left Before %s"%vels(target_linear_vel,target_angular_vel))
+    print("Move the Robot anti clockwise %s"%vels(target_linear_vel,target_angular_vel))
     status = status + 1
 
+    # Create a twist object
     twist = Twist()
     control_linear_vel = makeSimpleProfile(control_linear_vel, target_linear_vel, (LIN_VEL_STEP_SIZE/2.0))
     twist.linear.x = control_linear_vel; twist.linear.y = 0.0; twist.linear.z = 0.0
     control_angular_vel = makeSimpleProfile(control_angular_vel, target_angular_vel, (ANG_VEL_STEP_SIZE/2.0))
     twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = control_angular_vel
+
+    # publishing the twist object to the topic with updated linear and angular velocity
     pub.publish(twist)
-    print("@@@@@@@@@@@@@@@@@ Left After %s"%vels(target_linear_vel,target_angular_vel))
     return "200"
 
+# /right api to move the robot clockwise
 @app.route("/right", methods = ["POST"])
 def keyRight():
-    print("Clicked on Right Button")
 
     target_linear_vel   = 0.0
     target_angular_vel  = 0.0
@@ -168,8 +181,10 @@ def keyRight():
     ANG_VEL_STEP_SIZE = 0.1
     status=0
     target_angular_vel = checkAngularLimitVelocity(target_angular_vel - ANG_VEL_STEP_SIZE)
-    print("@@@@@@@@@@@@@@@@@ Right Before %s"%vels(target_linear_vel,target_angular_vel))
+    print("Moving the Robot clockwise %s"%vels(target_linear_vel,target_angular_vel))
     status = status + 1
+
+    # Create a twist object
     twist = Twist()
 
     control_linear_vel = makeSimpleProfile(control_linear_vel, target_linear_vel, (LIN_VEL_STEP_SIZE/2.0))
@@ -178,33 +193,35 @@ def keyRight():
     control_angular_vel = makeSimpleProfile(control_angular_vel, target_angular_vel, (ANG_VEL_STEP_SIZE/2.0))
     twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = control_angular_vel
 
+    # publishing the twist object to the topic with updated linear and angular velocity
     pub.publish(twist)
-    print("@@@@@@@@@@@@@@@@@ Right After %s"%vels(target_linear_vel,target_angular_vel))
     return "200"
 
+# /reset api to move the robot to its base location
 @app.route("/reset", methods = ["POST"])
 def keyReset():
-    print("Clicked on Reset Button")
 
     target_linear_vel   = 0.0
     control_linear_vel  = 0.0
     target_angular_vel  = 0.0
     control_angular_vel = 0.0
 
-    print("@@@@@@@@@@@@@@@@@ Reset Before %s"%vels(target_linear_vel,target_angular_vel))
+    print("Move the Robot to its base location %s"%vels(target_linear_vel,target_angular_vel))
 
     twist = Twist()
     twist.linear.x = 0.0; twist.linear.y = 0.0; twist.linear.z = 0.0
     twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = 0.0
+
+    # publishing the twist object to the topic with updated linear and angular velocity
     pub.publish(twist)
-    print("@@@@@@@@@@@@@@@@@ Reset After %s"%vels(target_linear_vel,target_angular_vel))
     return "200"
 
 @app.route("/")
 def homePage():
+    #rendering index.html as a base url
     return render_template("index.html")
 
 if __name__ == "__main__":
-    print('ItsDangerous version',it.__version__)
-    print('Markup Version',mk.__version__)
+
+    # Setting the application to run on port 9000
     app.run(host="localhost", port=9000, debug=True)
